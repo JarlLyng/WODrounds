911af75baa3e460a1aea80e8f3a2fd74dc593573
Jarl Lyng <jarllyng@gmail.com>
2026-02-15 23:06:39 +0100

    Initial Commit

---
+++ WODrounds/WODroundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WODroundsApp.swift
+//  WODrounds
+//
+//  Created by Jarl Lyng on 15/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct WODroundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
